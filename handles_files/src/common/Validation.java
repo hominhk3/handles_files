969a5c665e3a3a83019e1b64b22916f55cfd9461
hominhk3 <144079318+hominhk3@users.noreply.github.com>
2023-10-13 16:19:04 +0700

    Add files via upload

---
+++ handles_files/src/common/Validation.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common;
+
+import static java.time.Clock.system;
+import java.util.Scanner;
+
+/**
+ *
+ * @author dell
+ */
+public class Validation {
+         private static final Scanner in = new Scanner(System.in);
+    public static int checkInputIntLimit(int min, int max) {
+        while (true) {
+            try {
+                int result = Integer.parseInt(in.nextLine().trim());
+                if (result < min || result > max) {
+                    throw new NumberFormatException();
+
+                }
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Please input number in rage [" + min + ", " + max + "]");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+    public static String checkInputPathFile() {
+        System.out.print("Enter path file: ");
+        while (true) {
+            String result = in.nextLine().trim();
+            if (result.isEmpty()) {
+                System.err.println("Not empty");
+                System.out.print("Enter again: ");
+            } else {
+                return result;
+            }
+        }
+    }
+    public static double checkInputMoney() {
+        System.out.print("Enter money: ");
+        while (true) {
+            try {
+                double result = Double.parseDouble(in.nextLine());
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Must be input double");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+        public int getInt(String promt, int m, int n) {
+        int a = -1;
+        while (true) {
+            System.out.print(promt);
+            try {
+                String s = in.nextLine();
+                a = Integer.parseInt(s);
+                if (a >= m && a <= n) {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a number between " + m + " and " + n);
+            }
+        }
+        return a;
+}
+}
